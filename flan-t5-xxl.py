cb3cdcd0c7d68286b76f5202d45302f2e651dbb1
“atyenoria” <info@nakajima-akinori.com>
2022-10-26 22:20:33 +0900

    add flan-t5

---
+++ flan-t5-xxl.py
@@ -0,0 +1,10 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl")
+
+input_text = "translate English to German: How old are you?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
